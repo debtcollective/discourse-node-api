25773be99092c38db6e065c5f3c47427bbdb3d03
Alexandre Marcondes <alex@marcondesian.net>
2018-04-07 11:19:32 -0400

    Add filtering user search

--- models/admin/users.js
+++ models/admin/users.js
@@ -3,6 +3,6 @@
  */
 module.exports = api => ({
   getById: id => api.authGet(`/admin/users/${id}.json`)(),
-  search: ({ flag = 'active', order = 'username', ascending = false, page = 1, show_emails = false }) =>
-    api.authGet(`/admin/users/list/${flag}.json`)({ order, ascending, page, show_emails }),
+  search: ({ flag = 'active', order = 'username', ascending = false, page = 1, show_emails = false, filter = null }) =>
+    api.authGet(`/admin/users/list/${flag}.json`)({ order, ascending, page, show_emails, filter }),
 });
