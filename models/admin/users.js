072516fb5f8caa070b9c22adaedf1544300df187
Alexandre Marcondes <alex@marcondesian.net>
2018-04-11 18:27:24 -0400

    Add ability to pass in custom parameters to users query

--- models/admin/users.js
+++ models/admin/users.js
@@ -3,6 +3,13 @@
  */
 module.exports = api => ({
   getById: id => api.authGet(`/admin/users/${id}.json`)(),
-  search: ({ flag = 'active', order = 'username', ascending = false, page = 1, show_emails = false, filter = null }) =>
-    api.authGet(`/admin/users/list/${flag}.json`)({ order, ascending, page, show_emails, filter }),
+  search: ({
+    flag = 'active',
+    order = 'username',
+    ascending = false,
+    page = 1,
+    show_emails = false,
+    filter = null,
+    params = {},
+  }) => api.authGet(`/admin/users/list/${flag}.json`)({ order, ascending, page, show_emails, filter, ...params }),
 });
