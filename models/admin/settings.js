7b3ba4f73b72757a29673bdea2958bcc302cda06
Shoshana Berleant <sberleant@gmail.com>
2018-06-09 18:31:11 -0700

    add sso settings

--- models/admin/settings.js
+++ models/admin/settings.js
@@ -1,6 +1,12 @@
 /**
  * @param {discourseApi.DiscourseApiShuttle} api
  */
+
+const path = '/admin/site_settings/';
+
 module.exports = api => ({
-  enableTags: enable => api.authPut('/admin/site_settings/tagging_enabled')({ tagging_enabled: enable }),
+  enableTags: enable => api.authPut(path + 'tagging_enabled')({ tagging_enabled: enable }),
+  enableSsoProvider: enable =>
+    api.authPut(path + 'enable_sso_provider')({ enable_sso_provider: enable }),
+  setSsoSecret: sso_secret => api.authPut(path + 'sso_secret')({ sso_secret }),
 });
