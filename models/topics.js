7fa552a3a0fb8bf013a1686f6919bb1a2d2b2efb
Alexandre Marcondes <alex@marcondesian.net>
2018-09-22 14:57:53 -0700

    Add invite handler

--- models/topics.js
+++ models/topics.js
@@ -5,5 +5,24 @@
 module.exports = api => ({
   get: id => api.authGet(`/t/${id}.json`)(),
   update: t => api.authPut(`/t/${t.slug}/${t.id}.json`)(t),
-  getPostId: ({ post_stream: { posts: [{ id }] } }) => id,
+  getPostId: ({
+    post_stream: {
+      posts: [{ id }],
+    },
+  }) => id,
+  /**
+   * Invite the passed in users or groups or email
+   * @param {number} topicId The topic id to invite to
+   * @param {?string} options.user An optional username
+   * @param {?string[]} options.groupNames An optional list of group names
+   * @param {?string} options.customMessage An optional custom message
+   * @param {?string} options.email An optional email
+   * @return {Promise<any>} A promise representing the resolved request
+   */
+  invite: (topicId, { user, groupNames, customMessage, email }) =>
+    api.authPost(`/t/${topicId}/invite`)({
+      ...(user ? { user } : {}),
+      ...(groupNames ? { group_names: groupNames } : {}),
+      ...(customMessage ? { custom_message: customMessage } : {}),
+    }),
 });
