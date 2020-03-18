c97f0a736fd0fbd4113eab296a0400accbac045e
Orlando Del Aguila <orlando@hashlabs.com>
2020-03-18 17:28:44 -0600

    dev: change url dash to underscore

--- models/badges.js
+++ models/badges.js
@@ -4,5 +4,5 @@
 module.exports = api => ({
   userBadges: username => api.authGet(`/user-badges/${username}.json`)(),
   assignBadgeToUser: ({ username, badge_id, reason }) =>
-    api.authPost(`/user-badges.json`)({ username, badge_id, reason }),
+    api.authPost(`/user_badges.json`)({ username, badge_id, reason }),
 });
